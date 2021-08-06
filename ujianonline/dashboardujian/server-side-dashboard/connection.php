ef4d507d562efd2a27178658c4614e734a9b0ab0
~IhsanMudzaffarAlBarqawi <65557996+nobodylikeme14@users.noreply.github.com>
2021-08-06 22:45:13 +0700

    Delete ujianonline directory

--- ujianonline/dashboardujian/server-side-dashboard/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-		$host ="localhost";
-    $user ="root";
-    $pass ="";
-    $db 	="db_ujian";
-    $conn = mysqli_connect($host, $user, $pass, $db) or die();
- ?>
--- ujianonline/dashboardujian/server-side-dashboard/tanggal.php
+++
@@ -1,5 +0,0 @@
-<?php
-include ("functiontanggal.php ");
-$date=date('Y-m-d');
-echo format_hari_tanggal($date);
-?>
