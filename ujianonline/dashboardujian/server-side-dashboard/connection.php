40e7f5b957be79ff6c7f3efa1c5d29d19aa1e874
~IhsanMudzaffarAlBarqawi <65557996+nobodylikeme14@users.noreply.github.com>
2021-08-06 22:43:45 +0700

    Add files via upload

---
+++ ujianonline/dashboardujian/server-side-dashboard/connection.php
@@ -0,0 +1,7 @@
+<?php
+		$host ="localhost";
+    $user ="root";
+    $pass ="";
+    $db 	="db_ujian";
+    $conn = mysqli_connect($host, $user, $pass, $db) or die();
+ ?>
---
+++ ujianonline/dashboardujian/server-side-dashboard/tanggal.php
@@ -0,0 +1,5 @@
+<?php
+include ("functiontanggal.php ");
+$date=date('Y-m-d');
+echo format_hari_tanggal($date);
+?>
