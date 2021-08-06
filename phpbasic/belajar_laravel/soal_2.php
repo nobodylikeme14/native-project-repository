8e609e4d15cee409ced94207d5e3819acec89ec7
~IhsanMudzaffarAlBarqawi <65557996+nobodylikeme14@users.noreply.github.com>
2021-08-06 22:52:34 +0700

    Delete phpbasic directory

--- phpbasic/belajar_laravel/soal_2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$input = "Saya suka mendengarkan musik.";
-echo "Input : ".$input."<br>";
-echo "Output : <br>";
-echo "Panjang Kata : ".str_word_count($input)."<br>";
-echo "Panjang Karakter : ".strlen($input)."<br>";
-
-?>
